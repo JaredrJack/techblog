d0a63c429690fbe35e57d559d2e93f81ecfa0e63
jared <jaredrjack93@gmail.com>
2023-06-11 22:15:54 -0400

    updated models and db

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
-const userRoutes = require('./api/userRoutes');
-const postRoutes = require('./api/postRoutes');
-const commentRoutes = require('./api/commentRoutes')
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
-router.use('/user', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes)
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
